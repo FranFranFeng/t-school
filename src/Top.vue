5b88ae0fab803e9886b60997168221561e7ad7cb
Yuning Feng <11335308@tschool.tp.edu.tw>
2025-01-08 23:53:06 +0800

    add navbar

--- src/Top.vue
+++ src/Top.vue
@@ -1,10 +1,14 @@
+<script setup>
+import NavBar from "./NavBar.vue";
+</script>
+
 <template>
   <nav
       class="flex z-10 w-full bg-blue-900 min-h-[16px] max-md:max-w-full"
       aria-label="Top border"
   ></nav>
   <header
-      class="flex justify-center items-center gap-5 px-6 py-px w-full text-3xl font-bold whitespace-nowrap bg-white text-slate-400 max-md:px-5 max-md:max-w-full"
+      class="flex justify-center items-center gap-5 px-5 py-px w-full text-3xl font-bold whitespace-nowrap bg-white text-slate-400 max-md:px-5 max-md:max-w-full"
   >
     <RouterLink class="menuItem-active-link" to="/">
       <img
@@ -23,29 +27,7 @@
         tabindex="0"
     >官網</a
     >
-
   </header>
-  <nav
-      class="flex gap-10 items-center px-8 w-full text-3xl text-blue-900 whitespace-nowrap bg-blue-200 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] max-md:px-5 max-md:max-w-full"
-      aria-label="Main navigation"
-  >
-    <button class="self-stretch py-2 my-auto w-[16%]" tabindex="0">
-      <RouterLink class="menuItem-active-link" to="/teaching">教學理念</RouterLink>
-    </button>
-    <button class="self-stretch py-2 my-auto w-[16%]" tabindex="0">
-      <RouterLink class="menuItem-active-link" to="/campus">校園資訊</RouterLink>
-    </button>
-    <button class="grow shrink self-stretch py-2 my-auto w-[16%]" tabindex="0">
-      <RouterLink class="menuItem-active-link" to="/lesson">課程介紹</RouterLink>
-    </button>
-    <button class="self-start py-2 my-auto w-[16%]" tabindex="0">
-      <RouterLink class="menuItem-active-link" to="/faq">FAQ</RouterLink>
-    </button>
-    <button class="grow shrink self-stretch py-2 my-auto w-[16%]" tabindex="0">
-      <RouterLink class="menuItem-active-link" to="/join">入學專欄</RouterLink>
-    </button>
-    <button class="grow shrink self-stretch py-2 my-auto w-[16%]" tabindex="0">
-      <RouterLink class="menuItem-active-link" to="/about">關於我們</RouterLink>
-    </button>
-  </nav>
+
+  <NavBar/>
 </template>
